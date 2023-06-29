420d8c62fc47a638f61fa44ee3c4e7d258800640
Miguel-Custodio <repmiguelito@gmail.com>
2023-06-29 16:27:10 -0300

    componentizando a aplicacao

---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { shallowReactive } from 'vue';
+
+    const props = defineProps(['tarefas'])
+</script>
+
+<template>
+    <ul v-if="props.tarefas.length > 0" class="list-group mt-4">
+        <li class="list-group-item" v-for="tarefa in props.tarefas">
+            <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
+                :id="tarefa.titulo" type="checkbox">
+            <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">
+                {{ tarefa.titulo }}
+            </label>
+        </li>
+    </ul>
+    <p v-else>Você não possui tarefas pendentes.</p>
+</template>
+
+<style scoped>
+    .done {
+        text-decoration: line-through;
+    }
+
+    p {
+        margin-top: 24px;
+        margin-left: 12px;
+    }
+</style>
